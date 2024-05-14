0c22f66be4ff08b64aa0a465768526ecbd842fd3
loveshiho <1822881521@qq.com>
2024-05-14 17:34:51 +0800

    项目搭建-项目依赖、配置文件、sql脚本

---
+++ community-management/src/test/java/com/akai/CommunityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
