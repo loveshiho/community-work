d74d759cbfadf79511d11496bafde1460265c3ab
loveshiho <1822881521@qq.com>
2024-05-15 09:04:26 +0800

    项目搭建-修改HjyCommunityController

--- community-management/src/main/java/com/akai/web/controller/HjyCommunityController.java
+++ community-management/src/main/java/com/akai/web/controller/HjyCommunityController.java
@@ -1,13 +1,12 @@
 package com.akai.web.controller;
 
-import com.akai.common.constant.HttpStatus;
+import com.akai.common.core.controller.BaseController;
 import com.akai.common.core.page.PageResult;
-import com.akai.common.utils.ServletUtils;
+
 import com.akai.community.domain.HjyCommunity;
 import com.akai.community.domain.dto.HjyCommunityDto;
 import com.akai.community.service.HjyCommunityService;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,22 +15,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/community")
-public class HjyCommunityController {
+public class HjyCommunityController extends BaseController {
     @Autowired
     private HjyCommunityService hjyCommunityService;
 
     @RequestMapping("/list")
     public PageResult list(HjyCommunity hjyCommunity) {
-        Integer pageSize = ServletUtils.getParameterToInt("pageSize");
-        Integer pageNum = ServletUtils.getParameterToInt("pageNum");
         // 使用 PageHelper
-        PageHelper.startPage(pageNum, pageSize);
+        startPage();
         List<HjyCommunityDto> list = hjyCommunityService.selectHjyCommunityList(hjyCommunity);
         // 封装数据
-        PageResult result = new PageResult(new PageInfo<>(list).getTotal(), list);
-        result.setCode(HttpStatus.SUCCESS);
-        result.setMessage("查询成功");
-
+        PageResult result = getData(list);
         // 响应结果
         return result;
     }
