45b37273dc18bbf164270239e2b7e70bb315d6cd
loveshiho <1822881521@qq.com>
2024-05-16 22:07:33 +0800

    springboot日志实战

---
+++ community-management/src/test/java/com/akai/log/testLog.java
@@ -0,0 +1,14 @@
+package com.akai.log;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// @SpringBootTest
+public class testLog {
+    @Test
+    public void testLog(){
+        Logger logger = LoggerFactory.getLogger(testLog.class);
+        logger.info("Hello LogBack!");
+    }
+}
